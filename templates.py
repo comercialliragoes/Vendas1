5979c6fc6736ead70523b4e91ea74137678da5a2
comercialliragoes <comercialliragoes@gmail.com>
2025-03-28 18:17:58 -0300

    Add files via upload

--- templates.py
+++ templates.py
@@ -2,9 +2,11 @@ import streamlit as st
 import pandas as pd
 from views import *
 from datetime import datetime
+import base64
+
+
 #use no terminal antes
 #streamlit cache clear
-
 
 def gerar_txt_venda(venda_id, nome_cliente, itens_venda):
     """Gera conteúdo TXT formatado para a venda"""
@@ -39,19 +41,21 @@ def limpar_pagina():
 if "sessao_Atual" not in st.session_state:
     st.session_state["sessao_Atual"] = "Vendas"
 
+
+
 st.title('Orçamentos')
 
 # Menu lateral
 menu = st.sidebar.selectbox(
     'Escolha uma Opção',
-    ['Lista de Vendas', 'Produtos','Vendas','Atualizar Precos','Importacao DeepSeek'],
-    index=['Lista de Vendas', 'Produtos','Vendas','Atualizar Precos','Importacao DeepSeek'].index(st.session_state["sessao_Atual"])
+    ['Lista de Vendas', 'Produtos','Vendas','Atualização de Venda','Atualizar Precos','Importacao DeepSeek'],
+    index=['Lista de Vendas', 'Produtos','Vendas','Atualização de Venda','Atualizar Precos','Importacao DeepSeek'].index(st.session_state["sessao_Atual"])
 )
 
 # Atualiza o session state com base na seleção do menu lateral
 if menu != st.session_state["sessao_Atual"]:
     st.session_state["sessao_Atual"] = menu
-    st.session_state["id_venda_atual"] = None  # Reseta o aluno sendo editado
+    st.session_state["id_venda_atual"] = None  
     st.rerun()  # Recarrega a aplicação
 
 
@@ -299,6 +303,7 @@ if st.session_state['sessao_Atual'] == "Lista de Vendas":
                     st.header(f'total R${totalVenda}')
             st.divider()
             
+           
             with col8:
                 listarItems = st.button('🖨️', key=f'imprimir_{venda.id}')
             if listarItems:
@@ -325,6 +330,155 @@ if st.session_state['sessao_Atual'] == "Lista de Vendas":
                         st.code(conteudo_txt, language='text')
                     else:
                         st.error("Venda não encontrada!")
+
+#pagina atualizacao de venda
+if st.session_state['sessao_Atual'] == "Atualização de Venda":
+    st.write('Atualização de Venda')
+    vendas = listar_vendas()
+    opcoes_vendas = [' ']  # Opção vazia
+    mapeamento_vendas = {}  # Guarda a relação {texto_exibido: id_venda}
+
+    for venda in vendas:
+        texto_exibido = f"{venda.id}      -{venda.nome_cliente}     -R${venda.Total_venda:.2f}  -{venda.forma_pagamento}"
+        opcoes_vendas.append(texto_exibido)
+        mapeamento_vendas[texto_exibido] = venda.id  # Mapeia o texto para o ID
+
+    # Exibe o selectbox
+    venda_selecionada = st.selectbox('Selecione a Venda para Editar', opcoes_vendas)
+
+    # Pega apenas o ID da venda selecionada
+    if venda_selecionada != ' ':
+        id_venda = mapeamento_vendas[venda_selecionada]
+        st.write(f"ID da venda selecionada: {id_venda}")
+        
+        id_venda_atual = id_venda
+        if id_venda_atual == 'sem vendas':
+            id_venda_atual = 1
+        st.session_state["id_venda_atual"] = id_venda_atual
+        st.write(f'Incluir itens na venda {id_venda_atual}')
+        st.write('Procurar')
+        procura_field =''
+        procura_field = st.text_input('Digite o código do produto')
+        st.write('Incluir')
+        if procura_field:
+            produto = consultar_produtos(procura_field)
+            if (len(produto)) != 1:
+                for i, item in enumerate(produto):  # Usar enumerate para obter um índice único
+                    
+                    col1, col2, col3, col4, col5, col6 = st.columns([1, 1, 1, 0.5, 0.5, 1])
+                    with col1:
+                        codigo_field = st.text_input('Codigo', value=(item.codigo+' '+item.codInterno), key=f'codigo_{i}')
+                    with col2:
+                        descricao_field = st.text(item.descricao)
+                        #descricao_field = st.text_input('Descricao', value=item.descricao, key=f'descricao_{i}')
+                    with col3:
+                        quantidade_field = st.number_input('Qtd', step=1, value=1, key=f'quantidade_{i}')
+                    with col4:
+                        preco_field = st.text_input('Preço', value=item.preco, key=f'preco_{i}')
+                    with col5:
+                        total_field = st.text_input('Total', value=quantidade_field * float(preco_field), key=f'total_{i}')
+                    with col6:
+                        st.write('incluir')
+                        # Adicionar uma key única para o botão de inclusão
+                        botao_salvar = st.button('✅', key=f'botao_{i}')
+                    #usa o conteiner para mostrar a mensagem sem está dentro da coluna
+                    # if deve estar um tab antes da criação do botao
+                    if botao_salvar:
+                        with st.container():
+                            # Criar o objeto Vendas_item e incluir na venda
+                            #venda_item = Vendas_item(numero_venda=id_venda_atual,codigo=codigo_field,descricao=descricao_field,preco=float(preco_field),quantidade=quantidade_field,total=float(total_field))
+                            venda_item = Vendas_item(numero_venda=id_venda_atual,codigo=codigo_field,descricao=item.descricao,preco=float(preco_field),quantidade=quantidade_field,total=float(total_field))
+                            incluir_venda_item(venda_item)
+                        st.success('Item incluído')
+            else:
+                col1,col2,col3,col4,col5,col6 = st.columns([1, 1, 1, 0.5, 0.5, 1])
+                with col1:
+                    codigo_field = st.text_input('Codigo',value=(produto[0].codigo+' '+produto[0].codInterno))
+                with col2:
+                    descricao_field = st.text(produto[0].descricao)
+                    #descricao_field = st.text_input('Descricao',value=produto[0].descricao)
+                with col3:
+                    quantidade_field = st.number_input('Qtd',step=1,value=1)
+                with col4:
+                    preco_field = st.text_input('Preço',value=produto[0].preco)
+                with col5:
+                    total_field = st.text_input('Total',value=quantidade_field * float(preco_field))
+                with col6:
+                        st.write('incluir')
+                        botao_salvar = st.button('✅')
+                # usa o conteiner para mostrar a mensagem sem está dentro da coluna
+                # if deve estar um tab antes da criação do botao
+                if botao_salvar:
+                    with st.container():
+                        venda_item = Vendas_item(numero_venda=id_venda_atual,codigo=codigo_field,descricao=produto[0].descricao,preco=float(preco_field),quantidade=quantidade_field,total=float(total_field))
+                        #venda_item = Vendas_item(numero_venda=id_venda_atual,codigo=codigo_field,descricao=descricao_field,preco=float(preco_field),quantidade=quantidade_field,total=float(total_field))
+                        incluir_venda_item(venda_item)
+                    st.success('Item incluído ')
+        st.divider()
+        st.header('Itens do Orcamento')
+        itens_venda = consultar_venda_items(id_venda_atual)
+        print(itens_venda)
+        totalVenda = 0
+        if itens_venda != 'Venda não existe':
+            for linha in itens_venda:
+                totalVenda+=float(linha.total)
+                col1,col2,col3,col4,col5,col6 = st.columns([2,3,1,1,1,1])
+                with col1:
+                    st.write(linha.codigo)
+                with col2:
+                    st.write(linha.descricao)
+                with col3:
+                    st.write(linha.preco)
+                with col4:
+                    st.write(linha.quantidade)
+                with col5:
+                    st.write(linha.total)
+                    #st.write(linha.id,linha.codigo,linha.descricao,linha.preco,linha.quantidade,'Valor',linha.total)
+                with col6:
+                    apagarItem= st.button(f"🗑️", key=linha.id)
+                    if apagarItem:
+                        apagar_venda_item(linha.id)
+                        st.write('item Excluído')
+                        st.rerun()
+        
+        st.header('Total do Orçamento')
+        st.header('Valor Total Orçamento       R${:.2f}'.format(totalVenda))
+        st.divider()
+
+        st.header('Recebimento')
+        
+        tipo_pagamento=st.selectbox('Escolha o Metodo de Pagamento',['Dinheiro','Pix','Cartão de Débito','Cartão de Crédito'])
+        if tipo_pagamento == 'Dinheiro':
+            valor_pago = st.text_input('Dinheiro Recebibo')
+            if valor_pago:
+                valor_pagoF = float(valor_pago)
+                st.header('Troco     R${:.2f}'.format(valor_pagoF-totalVenda))
+                
+                confirmar_venda = st.button('Atualizar Venda')
+                if confirmar_venda:
+                    #venda = Vendas(nome_cliente=nome_cliente,Total_venda=totalVenda,forma_pagamento=tipo_pagamento)
+                    alterar_vendas(id_venda_atual, totalVenda , tipo_pagamento)
+                    #incluir_venda(venda)
+                    st.write('Recebimento em Dinheiro')
+                    limpar_pagina()
+                
+            else:
+                st.write('Aguardando Pagamento')
+        else:
+            
+            confirmar_venda = st.button('Confimar Alteração')
+            if confirmar_venda:
+                #venda = Vendas(nome_cliente=nome_cliente,Total_venda=totalVenda,forma_pagamento=tipo_pagamento)
+                st.write(f'Recebimento em {tipo_pagamento}')
+                alterar_vendas(id_venda_atual, totalVenda, tipo_pagamento)
+                #incluir_venda(venda)
+                st.write('Venda Atualizada')
+                limpar_pagina()
+           
+    else:
+        st.warning("Nenhuma venda selecionada.")
+
+
 #pagina atualizar preços
 if st.session_state['sessao_Atual'] == "Atualizar Precos":
     st.write('Atualizar Precos')
@@ -385,3 +539,4 @@ if st.session_state['sessao_Atual'] == "Importacao DeepSeek":
             st.error(f"Erro durante a importação: {str(e)}")
     else:
         st.warning('Por favor, envie o arquivo "Produtos.xlsx"')
+
